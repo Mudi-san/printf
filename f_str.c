84f54269bd876e621254ba84d040e7b794773661
Stevesahmed144 <abdellatifwork144@gmail.com>
2023-07-23 14:18:06 -0700

    Steve

--- f_str.c
+++ f_str.c
@@ -1,8 +1,4 @@
 #include "main.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdbool.h>
-#include <ctype.h>
 
 /**
  * get_precision - gets the precision from the format string
@@ -12,41 +8,23 @@
  *
  * Return: new pointer
  */
-int get_precision(const char *format, va_list args)
+int get_precision(char *p, params_t *params, va_list ap)
 {
-    int precision = -1;
-    const char *p = format;
-    bool var_precision = false;
+	int d = 0;
 
-    while (*p != '\0')
-    {
-        if (*p == '.')
+	if (*p != '.')
+		return (p);
+	p++;
+	if (*p == '*')
 	{
-            p++;
-            if (*p == '*')
-	    {
-                precision = va_arg(args, int);
-                var_precision = true;
-                p++;
-            }
-	    else if (isdigit(*p))
-	    {
-                precision = strtol(p, (char**)&p, 10);
-            }
-	    else
-	    {
-                precision = 0;
-            }
-            break;
-        }
+		d = va_arg(ap, int);
+		p++;
+	}
 	else
 	{
-            p++;
-        }
-    }
-    if (!var_precision && precision == -1)
-    {
-        precision = 0;
-    }
-    return precision;
+		while (_isdigit(*p))
+			d = d * 10 + (*p++ - '0');
+	}
+	params->precision = d;
+	return (p);
 }
